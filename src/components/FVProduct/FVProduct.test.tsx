b11182841d800cfc23eb55cb9d4ee4916177022c
Dejan Belusic <dejanbelusic@gmail.com>
2023-01-02 13:31:16 +0100

    Assignment 2 solution

--- src/components/FVProduct/FVProduct.test.tsx
+++ src/components/FVProduct/FVProduct.test.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FVProduct from '.';
+import { createProductMock } from '../../shared/product.mock';
+import FVRating from '../FVRating';
 
 describe('<FVProduct />', () => {
-  it('it should mount', () => {
-    render(
-      <FVProduct
-        id={0}
-        title={''}
-        price={0}
-        description={''}
-        category={''}
-        image={''}
-        rating={{
-          rate: 0,
-          count: 0
-        }}
-      />
-    );
+  const productMock = createProductMock({
+    rating: {
+      count: 34,
+      rate: 4.8
+    }
+  });
 
-    const product = screen.getByTestId('FVProduct');
+  it('should render product title, category, description and price', () => {
+    render(<FVProduct {...productMock} />);
 
-    expect(product).toBeInTheDocument();
+    expect(screen.getByText(productMock.title)).toBeInTheDocument();
+    expect(screen.getByText(productMock.category)).toBeInTheDocument();
+    expect(screen.getByText(productMock.description)).toBeInTheDocument();
+    expect(screen.getByText(productMock.price)).toBeInTheDocument();
+  });
+
+  it('should render a rating', () => {
+    render(<FVProduct {...productMock} />);
+
+    const ratingRender = render(<FVRating />);
+    const rating = ratingRender.container.querySelector('.rating');
+
+    expect(rating).toBeInTheDocument();
+  });
+
+  it('should render a square image with src and alt', () => {
+    const { container } = render(<FVProduct {...productMock} />);
+    const image = container.querySelector('img');
+
+    expect(image).toBeInTheDocument();
+    expect(image?.src).toContain(productMock.image);
+    expect(image?.alt).toContain(productMock.title);
   });
 });
--- src/components/FVProductsList/FVProductsList.test.tsx
+++ src/components/FVProductsList/FVProductsList.test.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
 import FVProductsList from './FVProductsList';
+import FVProduct from '../FVProduct';
+import { Product } from '../../types/product';
+import { createProductMock } from '../../shared/product.mock';
 
 describe('<FVProductsList />', () => {
-  it('it should mount', () => {
-    render(<FVProductsList productsList={[]} />);
+  const productsMock: Product[] = [
+    createProductMock({
+      id: 1
+    }),
+    createProductMock({
+      id: 2
+    }),
+    createProductMock({
+      id: 3
+    })
+  ];
 
+  it('should render 3 products', () => {
+    render(<FVProductsList productsList={productsMock} />);
     const products = screen.getByTestId('FVProductsList');
+    const productItems = products.querySelectorAll('.product');
 
-    expect(products).toBeInTheDocument();
+    expect(productItems.length).toBe(3);
+
+    productItems.forEach((item, index) => {
+      const product = render(<FVProduct {...productsMock[index]} />);
+      const productComponent = product.container.querySelector('.product');
+
+      expect(item).toStrictEqual(productComponent);
+    });
   });
 });
